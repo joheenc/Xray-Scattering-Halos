911ab4fe0485cb65f4151d9e991096ad66783c07
Joheen Chakraborty <joheen.c@gmail.com>
2019-11-24 13:53:25 -0500

    Update fuzzyhough.c

--- fuzzyhough.c
+++ fuzzyhough.c
@@ -4,52 +4,43 @@
 #include <math.h>
 
 double dist(int ay, int ax, int by, int bx) {
-	return sqrt(pow(ax-bx, 2.0)-pow(ay-by, 2.0));
+	return sqrt(pow(ax-bx, 2.0)+pow(ay-by, 2.0));
 }
 
 
-int *hough(int rows, int cols, int img[rows][cols], double radius, int fuzzy, int startx, int starty, int endx, int endy, int centerstep, int cutoff) {
-	int (*accumulator)[cols] = malloc(sizeof(int[rows][cols]));
+int *hough(int rows, int cols, int img[rows][cols], double radius, int fuzzy, int xs[], int ys[], int photonCount, int cutoff) {
+	// int (*accumulator)[cols] = malloc(sizeof(int[rows][cols]));
+	int *accumulator = malloc(photonCount * sizeof(int));
 	int acc;
-	for (int r = startx; r < endx; r += centerstep) {
-		for (int c = starty; c < endy; c += centerstep) {
-			acc = 0;
-			for (int a = r - radius - fuzzy; a < r + radius + fuzzy; a++) {
-				for (int b = c - radius - fuzzy; b < r + radius + fuzzy; b++) {
-					double distance = dist(a, b, r, c);
-					if (img[a][b] == 1 && distance >= radius-fuzzy && distance <= radius+fuzzy)
-						acc++;
-				}
-			}
-			accumulator[r][c] = acc;
-		}
-	}
+	printf("Generating Accumulator\n");
 
-	int returnPointCount = 0;
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < cols; j++) {
-			if (accumulator[i][j] >= cutoff)
-				returnPointCount++;
-		}
-	}
-
-	int *returnPoints = malloc(returnPointCount * sizeof(int) * 2);
-	int count = 0;
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < cols; j++) {
-			if (accumulator[i][j] >= cutoff) {
-				returnPoints[count++] = i;
-				returnPoints[count++] = j;
-				printf("%d, %d, rad=%f\n", i, j, radius);
+	for (int a = 0; a < photonCount; a++) {
+		int centerX = xs[a];
+		int centerY = ys[a];
+		int acc = 0;
+		printf("Center: (%d, %d)\n", centerX, centerY);
+		for (int b = 0; b < photonCount; b++) {
+			int photonX = xs[b];
+			int photonY = ys[b];
+			if (a != b) {
+				 int distance = dist(photonY, photonX, centerY, centerX);
+				 printf("[%d/%d] Checking photon: (%d, %d), dist = %d\n", a, photonCount, photonX, photonY, distance);
+				 if (distance >= radius-fuzzy && distance < radius+fuzzy)
+					 acc++;
 			}
 		}
+		accumulator[a] = acc;
 	}
-	
-	free(accumulator);
-	return returnPoints;
+
+	printf("Accumulator created: \n");
+	for (int i = 0; i < photonCount; i++)
+		printf("%d ", accumulator[i]);
+	// free(accumulator);
+	return accumulator;
 }
-int main() {
-	char* filename = "normimg.txt";
+
+int main(int argc, char **argv) {
+	char* filename = argv[1];
 	char buf[4096];
 	FILE *fp;
 
@@ -60,13 +51,14 @@ int main() {
 
 	int rows = 0;
 	int cols = 0;
-	while (fgets(buf, sizeof(buf), fp) != NULL) {
-		if (rows == 1)
+	int numPhotons = 0;
+	while (fgets(buf, sizeof(buf), fp) != NULL) {	//get dimensions of image, store in rows & cols
+		if (rows == 1) //just so we only do this count once
 			cols = strlen(buf)-1;
 		rows++;
 	}
 
-	int (*img)[cols] = malloc(sizeof(int[rows][cols]));
+	int (*img)[cols] = malloc(sizeof(int[rows][cols])); //stores image in 2d int array img[][]
 	fseek(fp, 0, SEEK_SET);
 	char *cp;
 	int r = 0;
@@ -74,17 +66,35 @@ int main() {
 	while (fgets(buf, sizeof(buf), fp) != NULL) {
 		c = 0;
 		cp = buf;
-		while (*cp)
-			img[r][c++] = *cp++ - 48;
+		while (*cp) {
+			int pix = *cp++ - 48;
+			img[r][c++] = pix;
+			if (pix > 0)
+				numPhotons++;
+		}
 		r++;
 	}
 
-	for (int rad = 10.0; rad < 200.0; rad+=10.0) {
-		int *houghCenters = hough(rows, cols, img, rad, 5, 0, 0, rows, cols, 3, 10);
-		free(houghCenters);
+	int photonxs[numPhotons], photonys[numPhotons];
+	int a = 0;
+	for (r = 0; r < rows; r++) {	//stores locations of photons in the image
+		for (c = 0; c < cols; c++) {
+			if (img[r][c] > 0) {
+				photonxs[a] = r;
+				photonys[a] = r;
+				a++;
+			}
+			if (a == numPhotons)
+				break;
+		}
+		if (a == numPhotons)
+			break;
 	}
+
+	int* houghCenters = hough(rows, cols, img, 50, 5, photonxs, photonys, numPhotons, 10);
+
 	fclose(fp);
 	free(img);
+	free(houghCenters);
 	return 0;
 }
-
