ee973647d3c78ddd8532814f381d5467e26f1dac
Hunter Holland <hunterholland@Hunters-MacBook-Pro-15.local>
2020-06-22 00:10:02 -0400

    Polished a bit

--- halo.py
+++ halo.py
@@ -1,11 +1,10 @@
 import os
 from ctypes import CDLL
 
-so_filename = "fuzzyhough_nottxt.so"
-os.system(
-    f"cd Pipelines/Xray-Scattering-Halos; gcc -shared -o {so_filename} -fPIC fuzzyhough_nottxt.c")
+filename = "fuzzyhough_notxt"
+os.system(f"gcc -shared -o {filename}.so -fPIC {filename}.c")
 
-so_filepath = f"/Users/hunterholland/Documents/Research/Laidlaw/Pipelines/Xray-Scattering-Halos/{so_filename}"
+so_filepath = f"/Users/hunterholland/Documents/Research/Laidlaw/Pipelines/Xray-Scattering-Halos/{filename}.so"
 halo_object = CDLL(so_filepath)
 
 # halo_object.hough(2darray)
