b4da3a39a3cdad2d0f04a2b103710f17bac53c6b
joheenc <joheen.c@gmail.com>
2020-06-22 01:07:58 -0400

    interfacing with ctypes

--- halo.py
+++ halo.py
@@ -1,11 +1,21 @@
+from ctypes import *
+from astropy.io import fits
+import numpy as np
 import os
-from ctypes import CDLL
 
-so_filename = "fuzzyhough_nottxt.so"
-os.system(
-    f"cd Pipelines/Xray-Scattering-Halos; gcc -shared -o {so_filename} -fPIC fuzzyhough_nottxt.c")
+hdul = fits.open('testfile.evt.gz')
+xs = hdul[1].data['X']
+ys = hdul[1].data['Y']
+photonCount = len(xs)
 
-so_filepath = f"/Users/hunterholland/Documents/Research/Laidlaw/Pipelines/Xray-Scattering-Halos/{so_filename}"
-halo_object = CDLL(so_filepath)
+filename = "fuzzyhough_notxt"
+filepath = "./"
 
-# halo_object.hough(2darray)
+os.system(f'gcc -c -fPIC -lm {filename}.c -o {filename}.o')
+os.system(f'gcc {filename}.o -shared -o {filename}.so')
+halo_object = CDLL(f'{filepath}{filename}.so')
+
+print(xs[0])
+c_int_p = POINTER(c_int)
+halo_object.hough.argtypes = [c_int_p, c_int_p, c_int]
+halo_object.hough(xs.ctypes.data_as(c_int_p), ys.ctypes.data_as(c_int_p), photonCount)
