8ee3a57bc7754c3d6b7142b21e961827f9509715
Joheen Chakraborty <joheen.c@gmail.com>
2020-06-22 00:24:05 -0400

    Update halo.py

--- halo.py
+++ halo.py
@@ -1,5 +1,11 @@
 import os
 from ctypes import CDLL
+from astropy.io import fits
+
+hdul = fits.open('event.evt.gz')
+xs = hdul[1].data['X']
+ys = hdul[1].data['Y']
+photonCount = len(xs)
 
 filename = "fuzzyhough_notxt"
 os.system(f"gcc -shared -o {filename}.so -fPIC {filename}.c")
@@ -7,4 +13,4 @@ os.system(f"gcc -shared -o {filename}.so -fPIC {filename}.c")
 so_filepath = f"/Users/hunterholland/Documents/Research/Laidlaw/Pipelines/Xray-Scattering-Halos/{filename}.so"
 halo_object = CDLL(so_filepath)
 
-# halo_object.hough(2darray)
+halo_object.hough(xs, yx, photonCount)
