f07f7038387eca39cd55a51804701a62bb757101
Joheen Chakraborty <joheen.c@gmail.com>
2020-09-17 14:16:59 -0400

    Update batchrun.py

--- batchrun.py
+++ batchrun.py
@@ -1,5 +1,5 @@
 import os
 from halo import halo
-datadir = '../data/obs/'
+datadir = '<your data directory goes here>'
 for fname in os.listdir(datadir):
     halo(datadir+fname, saveimg=True, outfile='outfiles/'+fname.replace('.evt.gz', '.png'))
